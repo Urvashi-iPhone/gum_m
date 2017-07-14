8f7c681756347694d30dcc554c022a4892b3ba68
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-07-14 15:13:03 +0530

    Initial Commit

---
+++ GetUMoneyMerchant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetUMoneyMerchant
+//
+//  Created by Tecksky Techonologies on 7/14/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetUMoneyMerchant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetUMoneyMerchant
+//
+//  Created by Tecksky Techonologies on 7/14/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
